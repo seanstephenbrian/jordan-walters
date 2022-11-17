4b13e8087faae368c899e234c44049f14eabb45d
sean stephen brian <seanstephenbrian@gmail.com>
2022-11-17 11:58:55 -0500

    clean up js before proceeding
    separate related functions into 'modules'

--- script.js
+++ script.js
@@ -1,139 +1,156 @@
-function addBorderEffectListeners(selector) {
-    if (window.innerWidth > 749) {
-        const elements = document.querySelectorAll(selector);
-        elements.forEach(element => {
-            element.addEventListener('mouseover', delayedBorderReveal);
-        });   
+// module for page rendering functions:
+const Render = (function() {
+
+    // nav-related methods:
+    function nav() {
+
+        // render nav elements:
+        const header = document.querySelector('header');
+        const nav = document.createElement('nav');
+        header.appendChild(nav);
+
+            const navItems = document.createElement('ul');
+            navItems.className = 'nav-items';
+            nav.appendChild(navItems);
+
+                const close = document.createElement('li');
+                close.classList.add('nav-item', 'close');
+                navItems.appendChild(close);
+
+                    const closeIcon = document.createElement('img');
+                    closeIcon.classList.add('close-icon');
+                    closeIcon.src = 'img/svg/close.svg';
+                    closeIcon.setAttribute('alt', 'close menu');
+                    close.appendChild(closeIcon);
+
+                const photography = document.createElement('li');
+                photography.textContent = 'photography';
+                photography.classList.add('nav-item', 'photography');
+                navItems.appendChild(photography);
+
+                const architecture = document.createElement('li');
+                architecture.textContent = 'architecture';
+                architecture.classList.add('nav-item', 'architecture');
+                navItems.appendChild(architecture);
+
+                const sculpture = document.createElement('li');
+                sculpture.textContent = 'sculpture';
+                sculpture.classList.add('nav-item', 'sculpture');
+                navItems.appendChild(sculpture);
+
+                const furniture = document.createElement('li');
+                furniture.textContent = 'furniture';
+                furniture.classList.add('nav-item', 'furniture');
+                navItems.appendChild(furniture);
+
+                const drawing = document.createElement('li');
+                drawing.textContent = 'drawing';
+                drawing.classList.add('nav-item', 'drawing');
+                navItems.appendChild(drawing);
+
+                const logoContainer = document.createElement('li');
+                logoContainer.className = 'logo-container';
+                navItems.appendChild(logoContainer);
+
+                    const logoImg = document.createElement('img');
+                    logoImg.classList.add('nav-logo');
+                    logoImg.src = 'img/logo/round.png';
+                    logoContainer.appendChild(logoImg);
+
+        assignColorsToNavElements();
+
+        // add click listener to close icon and JW icon:
+        closeIcon.addEventListener('click', closeNav);
+        logoImg.addEventListener('click', closeNav);
+
+        Effects.addBorderEffectListeners('.close-icon');
+        Effects.addBorderEffectListeners('.nav-logo');
+
+        const html = document.querySelector('html');
+        html.style.overflowY = 'hidden';
+        const body = document.querySelector('body');
+        body.style.overflowY = 'hidden';
     }
-}
 
+    function assignColorsToNavElements() {
+        const root = document.documentElement;
+    
+        root.style.setProperty('--photography-hover', Effects.getRandomColor());
+        root.style.setProperty('--architecture-hover', Effects.getRandomColor());
+        root.style.setProperty('--sculpture-hover', Effects.getRandomColor());
+        root.style.setProperty('--furniture-hover', Effects.getRandomColor());
+        root.style.setProperty('--drawing-hover', Effects.getRandomColor());
+    
+        root.style.setProperty('--photography-hover-border', Effects.getRandomColor());
+        root.style.setProperty('--architecture-hover-border', Effects.getRandomColor());
+        root.style.setProperty('--sculpture-hover-border', Effects.getRandomColor());
+        root.style.setProperty('--furniture-hover-border', Effects.getRandomColor());
+        root.style.setProperty('--drawing-hover-border', Effects.getRandomColor());
+    }
 
-function delayedBorderReveal(e) {
-    const element = e.target;
+    function closeNav() {
+        const nav = document.querySelector('nav');
+        nav.remove();
+    
+        const html = document.querySelector('html');
+        html.style.overflowY = 'initial';
+        const body = document.querySelector('body');
+        body.style.overflowY = 'initial';
+    }
 
-    setTimeout(() => {
-        element.style.color = getRandomColor();
-        element.style.border = '0.5px solid ' + getRandomColor();
-        
+    return {
+        nav
+    }
+
+})();
+
+// module for special effects:
+const Effects = (function() {
+
+    function delayedBorderReveal(e) {
+        const element = e.target;
+    
         setTimeout(() => {
-            element.style.color = '';
-            element.style.border = '';
-        }, 400);
+            element.style.color = getRandomColor();
+            element.style.border = '0.5px solid ' + getRandomColor();
+            
+            setTimeout(() => {
+                element.style.color = '';
+                element.style.border = '';
+            }, 400);
+    
+        }, 100);
+    }
 
-    }, 100);
-}
+    function addBorderEffectListeners(selector) {
+        if (window.innerWidth > 749) {
+            const elements = document.querySelectorAll(selector);
+            elements.forEach(element => {
+                element.addEventListener('mouseover', delayedBorderReveal);
+            });   
+        }
+    }
 
-function getRandomColor() {
-    return '#' + Math.floor(Math.random()*16777215).toString(16);
-}
+    function getRandomColor() {
+        return '#' + Math.floor(Math.random()*16777215).toString(16);
+    }
 
-addBorderEffectListeners('header *');
+    addBorderEffectListeners('header *');
 
+    return {
+        getRandomColor,
+        addBorderEffectListeners
+    }
 
-function renderNav() {
+})();
 
-    // render nav elements:
-    const header = document.querySelector('header');
-    const nav = document.createElement('nav');
-    header.appendChild(nav);
-
-        const navItems = document.createElement('ul');
-        navItems.className = 'nav-items';
-        nav.appendChild(navItems);
-
-            const close = document.createElement('li');
-            close.classList.add('nav-item', 'close');
-            navItems.appendChild(close);
-
-                const closeIcon = document.createElement('img');
-                closeIcon.classList.add('close-icon');
-                closeIcon.src = 'img/svg/close.svg';
-                closeIcon.setAttribute('alt', 'close menu');
-                close.appendChild(closeIcon);
-
-            const photography = document.createElement('li');
-            photography.textContent = 'photography';
-            photography.classList.add('nav-item', 'photography');
-            navItems.appendChild(photography);
-
-            const architecture = document.createElement('li');
-            architecture.textContent = 'architecture';
-            architecture.classList.add('nav-item', 'architecture');
-            navItems.appendChild(architecture);
-
-            const sculpture = document.createElement('li');
-            sculpture.textContent = 'sculpture';
-            sculpture.classList.add('nav-item', 'sculpture');
-            navItems.appendChild(sculpture);
-
-            const furniture = document.createElement('li');
-            furniture.textContent = 'furniture';
-            furniture.classList.add('nav-item', 'furniture');
-            navItems.appendChild(furniture);
-
-            const drawing = document.createElement('li');
-            drawing.textContent = 'drawing';
-            drawing.classList.add('nav-item', 'drawing');
-            navItems.appendChild(drawing);
-
-            const logoContainer = document.createElement('li');
-            logoContainer.className = 'logo-container';
-            navItems.appendChild(logoContainer);
-
-                const logoImg = document.createElement('img');
-                logoImg.classList.add('nav-logo');
-                logoImg.src = 'img/logo/round.png';
-                logoContainer.appendChild(logoImg);
-
-    assignRandomColorsToNavElements();
-
-    // add click listener to close icon and JW icon:
-    closeIcon.addEventListener('click', closeNav);
-    logoImg.addEventListener('click', closeNav);
-
-    addBorderEffectListeners('.close-icon');
-    addBorderEffectListeners('.nav-logo');
-
-    const html = document.querySelector('html');
-    html.style.overflowY = 'hidden';
-    const body = document.querySelector('body');
-    body.style.overflowY = 'hidden';
-
-}
-
-function assignRandomColorsToNavElements() {
-    const root = document.documentElement;
-
-    root.style.setProperty('--photography-hover', getRandomColor());
-    root.style.setProperty('--architecture-hover', getRandomColor());
-    root.style.setProperty('--sculpture-hover', getRandomColor());
-    root.style.setProperty('--furniture-hover', getRandomColor());
-    root.style.setProperty('--drawing-hover', getRandomColor());
-
-    root.style.setProperty('--photography-hover-border', getRandomColor());
-    root.style.setProperty('--architecture-hover-border', getRandomColor());
-    root.style.setProperty('--sculpture-hover-border', getRandomColor());
-    root.style.setProperty('--furniture-hover-border', getRandomColor());
-    root.style.setProperty('--drawing-hover-border', getRandomColor());
-}
-
-function closeNav() {
-    const nav = document.querySelector('nav');
-    nav.remove();
-
-    const html = document.querySelector('html');
-    html.style.overflowY = 'initial';
-    const body = document.querySelector('body');
-    body.style.overflowY = 'initial';
-}
-
-(function addInitialListeners() {
-    // add click listener to header elements:
-    const logo = document.querySelector('.logo-container');
-    logo.addEventListener('click', renderNav);
-    const jordan = document.querySelector('.jordan');
-    jordan.addEventListener('click', renderNav);
-    const walters = document.querySelector('.walters');
-    walters.addEventListener('click', renderNav);
-
+// IIFE for code to run on page load:
+(function addInitialsListeners() {
+        // add click listener to header elements:
+        const logo = document.querySelector('.logo-container');
+        logo.addEventListener('click', Render.nav);
+        const jordan = document.querySelector('.jordan');
+        jordan.addEventListener('click', Render.nav);
+        const walters = document.querySelector('.walters');
+        walters.addEventListener('click', Render.nav);
 })();
